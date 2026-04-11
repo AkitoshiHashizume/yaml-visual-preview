b6654664138e526b04022ab0e08bcbc51bd92989
AkitoshiHashizume <aki.application0101@gmail.com>
2026-04-11 17:57:09 +0900

    delete-untilBuild

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "yaml.visual.preview.plugin.hashizume.online"
-version = "1.0.4"
+version = "1.0.5"
 
 repositories {
     mavenCentral()
@@ -31,7 +31,7 @@ intellijPlatform {
     pluginConfiguration {
         ideaVersion {
             sinceBuild = "241"
-            untilBuild = "253.*"
+
         }
     }
     signing {
